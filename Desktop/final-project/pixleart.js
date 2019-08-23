2c72fc42c6ea1978e425cbc382888898609380bb
tasnim eltonsy <tasneem.eltonsi>
2019-08-23 15:38:42 +0300

    updating

--- Desktop/final-project/pixleart.js
+++ Desktop/final-project/pixleart.js
@@ -1,31 +1,76 @@
+// $(function() {
+//     var theGrid = $("#pixel_canvas");
+//     var currentColor = "#000";
+//     $("#sizePicker").submit(function(e) {
+//       e.preventDefault();
+//       makeGrid();
+//     });
+//     $("#colorPicker").change(function() {
+//       currentColor = $(this).val();
+//     });
+//     theGrid.on("mousemove", "td", function(e) {
+//       if (e.buttons == 1) {
+//         $(this).css("background-color", currentColor); 
+//       }
+//     });
+//     theGrid.on("click", "td", function(e) {
+//       $(this).css("background-color", currentColor); 
+//     });
+//     function makeGrid() {
+//       var gridHeight = $("#input_height").val();
+//       var gridWidth = $("#input_width").val();
+//       theGrid.empty(); 
+//       for (x = 0; x <= gridHeight - 1; x++) {
+//         theGrid.append("<tr>"); 
+//         for (y = 0; y <= gridWidth - 1; y++) {
+//           $("tr:eq(" + x + ")").append("<td></td>");
+//         }
+//         theGrid.append("</tr>"); 
+//       }
+//     }
+//   });
 $(function() {
-    var theGrid = $("#pixel_canvas");
-    var currentColor = "#000";
-    $("#sizePicker").submit(function(e) {
-      e.preventDefault();
-      makeGrid();
-    });
-    $("#colorPicker").change(function() {
-      currentColor = $(this).val();
-    });
-    theGrid.on("mousemove", "td", function(e) {
-      if (e.buttons == 1) {
-        $(this).css("background-color", currentColor); 
-      }
-    });
-    theGrid.on("click", "td", function(e) {
-      $(this).css("background-color", currentColor); 
-    });
-    function makeGrid() {
-      var gridHeight = $("#input_height").val();
-      var gridWidth = $("#input_width").val();
-      theGrid.empty(); 
-      for (x = 0; x <= gridHeight - 1; x++) {
-        theGrid.append("<tr>"); 
-        for (y = 0; y <= gridWidth - 1; y++) {
-          $("tr:eq(" + x + ")").append("<td></td>");
-        }
-        theGrid.append("</tr>"); 
-      }
+  //Set the grid and current color to #000
+  var theGrid = $("#pixel_canvas");
+  var currentColor = "#000";
+
+  // Prevent default submit behavior and call makeGrid function
+  $("#sizePicker").submit(function(e) {
+    e.preventDefault();
+    makeGrid();
+  });
+
+  //Change the currentColor to the selected one
+  $("#colorPicker").change(function() {
+    currentColor = $(this).val();
+  });
+
+  //If the user clicks or moves the mouse in the grid
+  //while holding the left mouse button down draw the pixels
+  theGrid.on("mousemove", "td", function(e) {
+    if (e.buttons == 1) {
+      $(this).css("background-color", currentColor); //Draw the td that the user hovers
     }
   });
+
+  theGrid.on("click", "td", function(e) {
+    $(this).css("background-color", currentColor); //Draw the td that the user clicks
+  });
+
+  //Build the grid
+  function makeGrid() {
+    //Set the grid dimensions;
+    var gridHeight = $("#input_height").val();
+    var gridWidth = $("#input_width").val();
+    theGrid.empty(); //Empty the grid and start over
+    //Loop -> Create rows
+    for (x = 0; x <= gridHeight - 1; x++) {
+      theGrid.append("<tr>"); //Start Row
+      //Loop -> Create columns for each row
+      for (y = 0; y <= gridWidth - 1; y++) {
+        $("tr:eq(" + x + ")").append("<td></td>");
+      }
+      theGrid.append("</tr>"); //End Row
+    }
+  }
+});
